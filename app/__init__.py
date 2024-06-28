f1ff252857245cca00a1340274c63fd36f4528a3
Jason Williams <thebotanist856@gmail.com>
2024-06-28 09:45:45 -0400

    Add Frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,17 @@
-from flask import Flask, session
+from flask import Flask, session, render_template, send_from_directory
 from flask_session import Session
 from flask_cors import CORS
 from config import Config
 from models import db
 from flask_migrate import Migrate
+import os
 
 from .signup import sign
 from .signin import thelog
 from .drinksapi import senddrinks
 from .ordersapi import orders
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend/dist', static_url_path='/')
 app.config.from_object(Config)
 
 db.init_app(app)
@@ -22,3 +23,11 @@ app.register_blueprint(sign)
 app.register_blueprint(thelog)
 app.register_blueprint(senddrinks)
 app.register_blueprint(orders)
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def serve_files(path):
+    return send_from_directory(app.static_folder, path)
