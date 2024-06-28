4585cc7df19994bf06d3facb1567234941d2c946
Jason Williams <thebotanist856@gmail.com>
2024-06-28 15:51:59 -0400

    moved init to app

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, session, render_template, send_from_directory
 from flask_session import Session
 from flask_cors import CORS
-from config import Config
 from models import db
+from config import Config
+
 from flask_migrate import Migrate
 import os
 
-from .signup import sign
-from .signin import thelog
-from .drinksapi import senddrinks
-from .ordersapi import orders
+from signup import sign
+from signin import thelog
+from drinksapi import senddrinks
+from ordersapi import orders
 
-app = Flask(__name__, static_folder='react-frontend/dist', template_folder='react-frontend/dist', static_url_path='')
+app = Flask(__name__, static_folder='app/react-frontend/dist', template_folder='app/react-frontend/dist', static_url_path='')
 app.config.from_object(Config)
 
 db.init_app(app)
